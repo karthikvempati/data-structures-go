38117ed97d05852bc24455e8225d628c83cb0515
Prathyusha <prathusha16r@gmail.com>
2021-05-01 21:12:17 -0500

    Implemented stack

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/karthikvempati/data-structures-go/stack"
+)
+
+func main() {
+	s := stack.Stack{}
+	s.Push(100)
+	s.Push(200)
+	s.Push(300)
+	fmt.Println(s)
+	fmt.Println(s.Pop())
+}
---
+++ stack/stack.go
@@ -0,0 +1,18 @@
+package stack
+
+type Stack struct {
+	items []int 
+}
+
+//Push
+func (s *Stack) Push(item int) {
+	s.items = append(s.items, item)
+}
+
+//Push
+func (s *Stack) Pop() int {
+	len := len(s.items) - 1
+	val := s.items[len]
+	s.items = s.items[:len-1]
+	return val
+}
